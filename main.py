055b66c6016ad775c54f4a2e2506b0ef654afdd3
Piotr Borczyk <howorang@gmail.com>
2019-04-08 23:51:45 +0200

    data loading

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 label_dict = {
     0: "neutral",
@@ -12,20 +13,45 @@ label_dict = {
     7: "surprise"
 }
 
+
 def load_images(startpath):
+    img_paths = []
+    img_labels = []
     for paths, dirs, files in os.walk(startpath):
         for f in files:
             fullpath = os.path.join(paths, f)
-            load_file_into_tf(fullpath)
+            label, path = get_label_path(fullpath)
+            img_labels.append(label)
+            img_paths.append(path)
+    return img_labels, img_paths
 
 
-def load_file_into_tf(path):
+def get_label_path(path):
     image_path = path.replace('data/Emotion', 'data/cohn-kanade-images').replace('_emotion.txt', '.png')
     fp = open(path, "r")
-    label = label_dict.get(int(float(fp.readline())))
+    label = int(float(fp.readline()))
     fp.close()
-    image = tf.image.decode_png(image_path)
-    return label, image
+    return label, image_path
 
 
-load_images("data/Emotion")
+def load_and_preprocess_image(path):
+    image = tf.read_file(path)
+    return preprocess_image(image)
+
+
+def preprocess_image(image):
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize_images(image, [192, 192])
+    image /= 255.0  # normalize to [0,1] range
+    return image
+
+
+labels, paths = load_images("data/Emotion")
+
+path_ds = tf.data.Dataset.from_tensor_slices(paths)
+image_ds = path_ds.map(load_and_preprocess_image)
+
+label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(labels, tf.int64))
+
+image_label_ds = tf.data.Dataset.zip((image_ds, label_ds))
+
