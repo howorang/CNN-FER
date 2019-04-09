f96cbdc64b299d871cf25e2fe2a8433f30468525
Piotr Borczyk <howorang@gmail.com>
2019-04-10 01:36:30 +0200

    scratchbook

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import data_loader
 import tensorflow as tf
+from tensorflow import keras
 
 dataset, length = data_loader.get_dataset()
+dataset = dataset.repeat(10)
+dataset = dataset.shuffle(buffer_size=20)
+length = length * 10
+
 
 VAL_SIZE = 0.15
 TST_SIZE = 0.15
@@ -10,9 +15,28 @@ TRN_SIZE = 0.70
 
 
 
-dataset = dataset.shuffle(buffer_size=20)
-train_dataset = dataset.take(int(TRN_SIZE * length))
-test_dataset = dataset.skip(int(TRN_SIZE * length))
+train_set_count = int(TRN_SIZE * length)
+train_dataset = dataset.take(train_set_count)
+test_dataset = dataset.skip(train_set_count)
 
-val_dataset = test_dataset.skip(int(VAL_SIZE * length))
-test_dataset = test_dataset.take(int(TST_SIZE * length))
+validation_set_count = int(VAL_SIZE * length)
+val_dataset = test_dataset.skip(validation_set_count)
+test_set_count = int(TST_SIZE * length)
+test_dataset = test_dataset.take(test_set_count)
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(192, 3)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+batches_count = int(length / 20)
+
+history = model.fit(x=train_dataset.make_one_shot_iterator(),
+                    validation_data=val_dataset.make_one_shot_iterator(),
+                    epochs=40,
+                    batch_size=20,
+                    verbose=1,
+                    steps_per_epoch=1,
+                    validation_steps=1)
