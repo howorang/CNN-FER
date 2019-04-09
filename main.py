0ca9f72db9179fb8f700f96a16789081c4dca0d1
Piotr Borczyk <howorang@gmail.com>
2019-04-10 01:43:02 +0200

    validation plotting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import data_loader
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from tensorflow import keras
 
 dataset, length = data_loader.get_dataset()
@@ -7,13 +8,9 @@ dataset = dataset.repeat(10)
 dataset = dataset.shuffle(buffer_size=20)
 length = length * 10
 
-
 VAL_SIZE = 0.15
 TST_SIZE = 0.15
 TRN_SIZE = 0.70
-
-
-
 
 train_set_count = int(TRN_SIZE * length)
 train_dataset = dataset.take(train_set_count)
@@ -40,3 +37,23 @@ history = model.fit(x=train_dataset.make_one_shot_iterator(),
                     verbose=1,
                     steps_per_epoch=1,
                     validation_steps=1)
+history_dict = history.history
+history_dict.keys()
+
+acc = history_dict['acc']
+val_acc = history_dict['val_acc']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
