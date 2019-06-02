a0b9ada384aba57491b3e8df2d54c1d6f7b091a9
Piotr Borczyk <howorang@gmail.com>
2019-06-02 20:43:35 +0200

    numpy

--- collab.py
+++ collab.py
@@ -2,6 +2,11 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow import keras
 import os
+import numpy as np
+
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.utils import to_categorical
 
 label_dict = {
     0: "neutral",
@@ -16,14 +21,9 @@ label_dict = {
 
 
 def get_dataset():
-    labels, imgs = load_images("/content/data/Emotion")
-    length = len(labels);
-    labels = tf.one_hot(labels, 8)
-
-    label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(labels, tf.int64))
-    image_ds = tf.data.Dataset.from_tensor_slices(imgs)
-    image_label_ds = tf.data.Dataset.zip((image_ds, label_ds))
-    return image_label_ds, length
+    labels, imgs = load_images("data/Emotion")
+    length = len(labels)
+    return labels, imgs, length
 
 
 def load_images(startpath):
@@ -32,20 +32,15 @@ def load_images(startpath):
     for paths, dirs, files in os.walk(startpath):
         for f in files:
             fullpath = os.path.join(paths, f)
-            label, path, path2, path3, path4 = get_label_path(fullpath, f)
-            img_labels.append(label)
-            img_labels.append(label)
-            img_labels.append(label)
-            img_labels.append(label)
-            imgs.append(load_and_preprocess_image(path))
-            imgs.append(load_and_preprocess_image(path2))
-            imgs.append(load_and_preprocess_image(path3))
-            imgs.append(load_and_preprocess_image(path4))
-    return img_labels, imgs
+            label, paths = get_label_path(fullpath, f)
+            for path in paths:
+                img_labels.append(to_categorical(label, 8))
+                imgs.append(load_and_preprocess_image(path))
+    return np.array(img_labels), np.array(imgs)
 
 
 def get_label_path(path, filename):
-    image_path = '/content/data/' + filename.replace('_emotion.txt', '.png')
+    image_path = 'data/output/' + filename.replace('_emotion.txt', '.png')
     image_path_t = image_path.replace('.png', '_translated.png')
     image_path_m = image_path.replace('.png', '_mirrored.png')
     image_path_r = image_path.replace('.png', '_rotated.png')
@@ -53,43 +48,30 @@ def get_label_path(path, filename):
     fp = open(path, "r")
     label = int(float(fp.readline()))
     fp.close()
-    return label, image_path, image_path_m, image_path_r, image_path_t
+    return label, [image_path, image_path_m, image_path_r, image_path_t]
 
 
 def load_and_preprocess_image(path):
-    image = tf.read_file(path)
+    image = load_img(path)
     return preprocess_image(image)
 
 
 def preprocess_image(image):
-    image = tf.image.decode_jpeg(image, channels=1)
-    image = tf.image.resize_images(image, [192, 192])
-    image /= 255.0  # normalize to [0,1] range
-    return image
+    image = image.convert('L')  # to grayscale
+    image = image.resize((192, 192))
+    arr = img_to_array(image)
+    arr = np.divide(arr, 255)
+    return arr
 
-dataset, length = get_dataset()
-dataset = dataset.repeat(10)
-dataset = dataset.shuffle(buffer_size=20)
-length = length * 10
+
+labels, images, length = get_dataset()
 print(length)
 VAL_SIZE = 0.15
 TST_SIZE = 0.15
 TRN_SIZE = 0.70
 
-train_set_count = int(TRN_SIZE * length)
-train_dataset = dataset.take(train_set_count)
-test_dataset = dataset.skip(train_set_count)
-
-validation_set_count = int(VAL_SIZE * length)
-val_dataset = test_dataset.skip(validation_set_count)
-test_set_count = int(TST_SIZE * length)
-test_dataset = test_dataset.take(test_set_count)
 
 batch_size = 20
-
-train_dataset = train_dataset.batch(batch_size)
-val_dataset = val_dataset.batch(batch_size)
-test_dataset = test_dataset.batch(batch_size)
 
 model = keras.Sequential([
     keras.layers.Flatten(batch_size=batch_size),
@@ -100,8 +82,9 @@ model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-history = model.fit(train_dataset.make_one_shot_iterator(),
-                    validation_data=val_dataset.make_one_shot_iterator(),
+history = model.fit(x=images,
+                    y=labels,
+                    validation_split=0.15,
                     epochs=20,
                     verbose=1,
                     steps_per_epoch=batch_size)
