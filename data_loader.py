3b26e8ba546a1a763d39a9dba1620f42f287b276
Piotr Borczyk <howorang@gmail.com>
2019-08-15 19:51:41 +0200

    Hyper param turing

---
+++ data_loader.py
@@ -0,0 +1,52 @@
+import os
+
+import numpy as np
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import load_img
+from keras.utils import to_categorical
+from keras_preprocessing.image import load_img
+
+IMAGE_PATH = "data/Emotion"
+OUTPUT_PATH = 'data/output/'
+
+def get_dataset():
+    labels, imgs = load_images(IMAGE_PATH)
+    return labels, imgs
+
+
+def load_images(startpath):
+    imgs = []
+    img_labels = []
+    for paths, dirs, files in os.walk(startpath):
+        for f in files:
+            fullpath = os.path.join(paths, f)
+            label, paths = get_label_path(fullpath, f)
+            for path in paths:
+                img_labels.append(to_categorical(label, 8))
+                imgs.append(load_and_preprocess_image(path))
+    return np.array(img_labels), np.array(imgs)
+
+
+def get_label_path(path, filename):
+    image_path = OUTPUT_PATH + filename.replace('_emotion.txt', '.png')
+    image_path_t = image_path.replace('.png', '_translated.png')
+    image_path_m = image_path.replace('.png', '_mirrored.png')
+    image_path_r = image_path.replace('.png', '_rotated.png')
+
+    fp = open(path, "r")
+    label = int(float(fp.readline()))
+    fp.close()
+    return label, [image_path, image_path_m, image_path_r, image_path_t]
+
+
+def load_and_preprocess_image(path):
+    image = load_img(path)
+    return preprocess_image(image)
+
+
+def preprocess_image(image):
+    image = image.convert('L')  # to grayscale
+    image = image.resize((192, 192))
+    arr = img_to_array(image)
+    arr = np.divide(arr, 255)
+    return arr
---
+++ hyperparam.py
@@ -0,0 +1,117 @@
+import numpy as np
+import tensorflow as tf
+from hyperas import optim
+from hyperas.distributions import choice
+from hyperopt import Trials, STATUS_OK, tpe
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+
+from data_loader import get_dataset
+
+
+def create_model(X_train, Y_train, X_test, Y_test):
+    model = keras.Sequential([
+        keras.layers.Conv2D(filters={{choice([32, 64, 128])}},
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            activation='relu',
+                            input_shape=(192, 192, 1)),
+        keras.layers.Conv2D(filters={{choice([32, 64, 128])}},
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            activation='relu'),
+        keras.layers.Conv2D(filters={{choice([32, 64, 128])}},
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            activation='relu'),
+        keras.layers.MaxPooling2D(pool_size=(2, 2),
+                                  padding='valid'),
+        keras.layers.Conv2D(filters={{choice([32, 64, 128])}},
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            activation='relu'),
+        keras.layers.Conv2D(filters={{choice([32, 64, 128])}},
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            activation='relu'),
+        keras.layers.Conv2D(filters={{choice([32, 64, 128])}},
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            activation='relu'),
+        keras.layers.MaxPooling2D(pool_size=(2, 2),
+                                  padding='valid'),
+        keras.layers.Conv2D(filters={{choice([32, 64, 128, 256])}},
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            activation='relu'),
+        keras.layers.Conv2D(filters={{choice([32, 64, 128, 256])}},
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            activation='relu'),
+        keras.layers.Conv2D(filters={{choice([32, 64, 128, 256])}},
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            activation='relu'),
+
+        keras.layers.MaxPooling2D(pool_size=(2, 2),
+                                  padding='valid'),
+        keras.layers.Conv2D(filters={{choice([32, 64, 128, 256])}},
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            activation='relu'),
+        keras.layers.Conv2D(filters={{choice([32, 64, 128, 256])}},
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            activation='relu'),
+        keras.layers.Conv2D(filters={{choice([32, 64, 128, 256])}},
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            activation='relu'),
+
+        keras.layers.MaxPooling2D(pool_size=(2, 2),
+                                  strides=(2, 2),
+                                  padding='valid'),
+
+        # first flatten
+        keras.layers.GlobalAveragePooling2D(),
+        keras.layers.Flatten(),
+        keras.layers.Dense(8, activation=tf.nn.softmax)
+    ])
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    result = model.fit(x=X_train,
+                       y=Y_train,
+                       validation_split=0.15,
+                       epochs=3,
+                       verbose=1,
+                       batch_size=32,
+                       shuffle=True)
+
+    validation_acc = np.amax(result.history['val_acc'])
+    print('Best validation acc of epoch:', validation_acc)
+    return {'loss': -validation_acc, 'status': STATUS_OK, 'model': model}
+
+
+def data():
+    labels, images = get_dataset()
+    X_train, X_test, Y_train, Y_test = train_test_split(images, labels, test_size=0.15)
+    X_train = np.repeat(X_train, 10, axis=0)
+    Y_train = np.repeat(Y_train, 10, axis=0)
+    return X_train, Y_train, X_test, Y_test
+
+
+if __name__ == '__main__':
+    best_run, best_model = optim.minimize(model=create_model,
+                                          data=data,
+                                          algo=tpe.suggest,
+                                          max_evals=5,
+                                          trials=Trials())
+    X_train, Y_train, X_test, Y_test = data()
+    print("Evalutation of best performing model:")
+    print(best_model.evaluate(X_test, Y_test))
+    print("Best performing model chosen hyper-parameters:")
+    print(best_run)
