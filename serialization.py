570b395bef7c72a54c45b3675b1fed72ee239884
Piotr Borczyk <howorang@gmail.com>
2019-09-21 23:51:18 +0200

    dataset serialization added

---
+++ serialization.py
@@ -0,0 +1,19 @@
+import datetime
+
+import h5py
+
+
+def save_dataset(path, labels, imgs):
+    filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    f = h5py.File(path + '/' + filename, "w")
+    f.create_dataset("labels", data=labels)
+    f.create_dataset("imgs", data=imgs)
+    f.close()
+
+
+def load_dataset(path):
+    f = h5py.File(path, "r")
+    labels = f['labels']
+    imgs = f['imgs']
+    f.close()
+    return labels, imgs
