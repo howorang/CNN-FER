78dfff9c3e4a2f0e7b8da5dae1aa47b66091c17d
Piotr Borczyk <howorang@gmail.com>
2019-11-11 17:31:21 +0100

    Shuffling data in set.

--- create_dataset.py
+++ create_dataset.py
@@ -6,7 +6,9 @@ import jaffe_loader
 import rafd_loader
 from heterogenic_loader import get_dataset
 from serialization import save_dataset
+from sklearn.utils import shuffle
 
-labels, imgs = get_dataset([jaffe_loader, rafd_loader, ck_loader], 1000)
+labels, imgs = get_dataset([jaffe_loader, rafd_loader, ck_loader], 600)
+labels, imgs = shuffle(labels, imgs)
 imgs = np.divide(imgs, 255)
 save_dataset("example", labels, imgs)
