4665e8a02f1b24a946bdb8e52f0339137508a466
Piotr Borczyk <howorang@gmail.com>
2019-12-05 00:33:33 +0100

    Hyper param logging to tensorboard.

--- collab.py
+++ collab.py
@@ -2,16 +2,12 @@ import datetime
 import os
 from enum import Enum
 
-import cv2
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers, models
-from keras.preprocessing.image import load_img
-from keras_preprocessing.image import load_img
-from sklearn.model_selection import train_test_split
-from tensorflow import keras
-import json
 from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from tensorboard.plugins.hparams import api as hp
+from tensorflow import keras
 
 from resnet import resnet50
 from serialization import load_dataset
@@ -24,39 +20,24 @@ class DatasetFiles(Enum):
     JAFFE = 'jaffe'
     HETERO = 'hetero'
 
+
 class Network(Enum):
     VGG19 = vgg19()
     RESNET = resnet50()
+
 
 class Optimizer(Enum):
     DEFAULT = keras.optimizers.Adam()
     LR = keras.optimizers.Adam(lr=0.00001)
 
-testing_scenerios = [
-    # (Network.RESNET, DatasetFiles.RAFD, Optimizer.DEFAULT),
-    # (Network.RESNET, DatasetFiles.RAFD, Optimizer.LR),
-    # (Network.VGG19, DatasetFiles.RAFD, Optimizer.DEFAULT),
 
-    # (Network.RESNET, DatasetFiles.JAFFE, Optimizer.DEFAULT),
-    # (Network.VGG19, DatasetFiles.JAFFE, Optimizer.DEFAULT),
-    # (Network.VGG19, DatasetFiles.JAFFE, Optimizer.LR),
-
-    # (Network.RESNET, DatasetFiles.HETERO, Optimizer.DEFAULT),
-    (Network.RESNET, DatasetFiles.HETERO, Optimizer.LR),
-    (Network.VGG19, DatasetFiles.HETERO, Optimizer.DEFAULT),
-    (Network.VGG19, DatasetFiles.HETERO, Optimizer.LR),
+HP_DATASET = hp.HParam('dataset', hp.Discrete({x.name for x in list(DatasetFiles)}))
+HP_OPTIMIZER = hp.HParam('optimizer', hp.Discrete({x.name for x in list(Optimizer)}))
+HP_NETWORK = hp.HParam('network', hp.Discrete({x.name for x in list(Network)}))
 
 
-]
-
-for scenerio in testing_scenerios:
-    print(scenerio[0].name + scenerio[1].name + scenerio[2].name)
-    dataset = scenerio[1]
-    network = scenerio[0]
-    optimizer = scenerio[2]
-
-
-    labels, images = load_dataset(dataset.value + '3000.h5')
+def run(run_name, hparams):
+    labels, images = load_dataset(hparams[HP_DATASET] + '3000.h5')
     # if 1 channel dataset
 
     labels = [to_categorical(i - 1, 6) for i in labels]
@@ -67,8 +48,6 @@ for scenerio in testing_scenerios:
         images = np.reshape(images, resotred_shape)
     # if 1 channel dataset
 
-
-
     images, X_test, labels, y_test = train_test_split(images, labels, test_size=0.15)
 
     labels = np.repeat(labels, 4, axis=0)
@@ -76,12 +55,12 @@ for scenerio in testing_scenerios:
 
     batch_size = 20
 
-    logdir = os.path.join("logs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S_" + network.name + "_" + dataset.name + "_" + optimizer.name))
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
+    logdir = os.path.join("logs", run_name)
+
     print(logdir)
 
-    model = network.value
-    model.compile(optimizer=optimizer.value,
+    model = Network[hparams[HP_NETWORK]].value
+    model.compile(optimizer=Optimizer[hparams[HP_OPTIMIZER]].value,
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
@@ -94,7 +73,8 @@ for scenerio in testing_scenerios:
                         verbose=1,
                         batch_size=32,
                         shuffle=True,
-                        callbacks=[tensorboard_callback])
+                        callbacks=[tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1),
+                                   hp.KerasCallback(logdir, hparams)])
 
     test_scalar_loss = model.evaluate(
         x=X_test,
@@ -103,6 +83,17 @@ for scenerio in testing_scenerios:
         verbose=1
     )
     model.save(os.path.join(logdir + os.path.sep + "model"))
-    print(str(test_scalar_loss),  file=open(os.path.join(logdir + os.path.sep + "scalar.txt"), "a"))
+    print(str(test_scalar_loss), file=open(os.path.join(logdir + os.path.sep + "scalar.txt"), "a"))
 
 
+for dataset in HP_DATASET.domain.values:
+    for network in HP_NETWORK.domain.values:
+        for optimizer in HP_OPTIMIZER.domain.values:
+            hparams = {
+                HP_DATASET: dataset,
+                HP_NETWORK: network,
+                HP_OPTIMIZER: optimizer,
+            }
+            run_name = hparams[HP_DATASET] + " " + hparams[HP_NETWORK] + " " + hparams[
+                HP_OPTIMIZER] + " " + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            run(run_name, hparams)
