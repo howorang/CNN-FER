e15cb7bcc9ae1b3f096211aef9844f819a15c19a
Piotr Borczyk <howorang@gmail.com>
2020-01-29 23:33:23 +0100

    using tf version of keras vis

--- visualize_filters.py
+++ visualize_filters.py
@@ -2,13 +2,17 @@ import math
 import os
 import tempfile
 
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from keras import activations
-from tensorflow.keras.models import load_model
-from vis.input_modifiers import Jitter
-from vis.utils import utils
-from vis.visualization import visualize_activation
+import tensorflow.keras as keras
+from tf_keras_vis.activation_maximization import ActivationMaximization
+from tf_keras_vis.activation_maximization import ActivationMaximization
+from tf_keras_vis.utils.losses import SmoothedLoss
+from tf_keras_vis.utils.callbacks import Print
+from resnet import resnet50
+import tensorflow as tf
+from tensorflow.keras import backend as K
 
 index_emotrion = {
     1: "ANGRY",
@@ -40,60 +44,52 @@ def plot_images(imgs, img_titles, dpi, title):
     plt.show()
 
 
-def apply_modifications_to_model(model):
-    """Applies modifications to the model layers to create a new Graph. For example, simply changing
-    `model.layers[idx].activation = new activation` does not change the graph. The entire graph needs to be updated
-    with modified inbound and outbound tensors because of change in layer building function.
-
-    Args:
-        model: The `keras.models.Model` instance.
-
-    Returns:
-        The modified model with changes applied. Does not mutate the original `model`.
-    """
-    # The strategy is to save the modified model and load it back. This is done because setting the activation
-    # in a Keras layer doesnt actually change the graph. We have to iterate the entire graph and change the
-    # layer inbound and outbound nodes with modified tensors. This is doubly complicated in Keras 2.x since
-    # multiple inbound and outbound nodes are allowed with the Graph API.
-    model_path = 'tmp/' + next(tempfile._get_candidate_names()) + '.h5'
-    try:
-        model.save(model_path)
-        return load_model(model_path)
-    finally:
-        os.remove(model_path)
-
 
 def visualize_conv_layer_filters(model, layer_name):
-    layer_idx = utils.find_layer_idx(model, layer_name)
-    categories = np.random.permutation(10)[:15]
+    def model_modifier(m):
+        new_model = tf.keras.Model(inputs=m.inputs, outputs=[m.get_layer(name=layer_name).output])
+        new_model.layers[-1].activation = tf.keras.activations.linear
+        return new_model
+
+    activation_maximization = ActivationMaximization(model, model_modifier)
+    num_of_filters = 16
+    filter_numbers = np.random.choice(model.get_layer(name=layer_name).output.shape[-1], num_of_filters)
     vis_images = []
-    image_modifiers = [Jitter(16)]
-    for idx in categories:
-        img = visualize_activation(model, layer_idx, filter_indices=idx, max_iter=1000, input_modifiers=image_modifiers)
-        img = np.squeeze(img, 2)
-        vis_images.append(img)
+    for filter_number in enumerate(filter_numbers):
+        # Define loss function that is sum of a filter output.
+        loss = SmoothedLoss(filter_number)
+
+        # Generate max activation
+        activation = activation_maximization(loss)
+        image = activation[0].astype(np.uint8)
+        vis_images.append(image)
     plot_images(vis_images, None, 400, layer_name)
 
 
-def visualize_dense_layer(model, layer_name, index_label_map):
-    layer_idx = utils.find_layer_idx(model, layer_name)
-    model.layers[layer_idx].activation = activations.linear
-    model = apply_modifications_to_model(model)
-
-    plt.rcParams['figure.figsize'] = (18, 6)
-
-    # 20 is the imagenet category for 'ouzel'
+def visualize_dense_layer(model, layer_name, index_label_map, itr):
+    def model_modifier(m):
+        m.layers[-1].activation = tf.keras.activations.linear
     imgs = []
     labels = []
     for index, label in index_label_map.items():
-        img = visualize_activation(model, layer_idx, filter_indices=index - 1, max_iter=500,
-                                   verbose=True)
+        activation_maximization = ActivationMaximization(model, model_modifier)
+        loss = lambda x: K.mean(x[:, index - 1])
+        activation = activation_maximization(loss, steps=itr,callbacks=[Print(interval=100)])
+        img = activation[0].astype(np.uint8)
         img = np.squeeze(img, 2)
+        cv2.imwrite(label + ".png", img)
         imgs.append(img)
         labels.append(label)
     plot_images(imgs, labels, 400, layer_name)
 
 
-model = load_model('HETERO RESNET DEFAULT 20200107-064749.h5')
-# visualize_conv_layer_filters(model, 'block1_conv2')
-visualize_dense_layer(model, 'fc1000', index_emotrion)
+model = resnet50()
+model.compile(optimizer=keras.optimizers.Adam(),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.load_weights('logs\\HETERO RESNET DEFAULT 20200127-082347\\model_weights.h5')
+# visualize_conv_layer_filters(model, 'conv1')
+
+
+def visualize(model, itr):
+    visualize_dense_layer(model, 'fc1000', index_emotrion, itr)
